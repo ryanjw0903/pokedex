1415f41be0d60edc3111e7c37982add36eaf55e5
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2020-06-17 10:02:15 -0500

    constructor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
 import PokedexHeader from './comp/PokedexHeader';
-function App() {
+
+class App extends React.Component {
+  constructor(props){
+      super(props)
+      this.state={
+        name: "Ryan"
+      }
+  }
+  render(){
   return (
-    <div className = "App ">
-      <PokedexHeader name = {"Ryan"} />
-    </div>
-  )
+      <div className = "App ">
+        <PokedexHeader name = {this.state.name} />
+      </div>
+    )
+  }
 }
 
 export default App;
