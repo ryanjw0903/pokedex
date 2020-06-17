872fc35cc99a534681a9ba1a53bf0e4985c56c9a
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2020-06-17 10:36:09 -0500

    input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import PokedexHeader from './comp/PokedexHeader';
+import {
+  Input,
+  Segment
+} from 'semantic-ui-react'
 
 class App extends React.Component {
   constructor(props){
@@ -8,11 +12,24 @@ class App extends React.Component {
       this.state={
         name: "Ryan"
       }
+      this.onChange = this.onChange.bind(this)
+  }
+  onChange(event,data){
+    this.setState({
+      name: data.value
+    })
   }
   render(){
   return (
       <div className = "App ">
         <PokedexHeader name = {this.state.name} />
+        <Segment  basic> 
+           <Input
+            label = "Input Pokemon name"
+            onChange = {this.onChange} 
+            value = {this.state.name}
+           /> 
+        </Segment>
       </div>
     )
   }
