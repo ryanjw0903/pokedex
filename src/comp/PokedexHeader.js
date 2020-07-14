e87781bad05048ed91e0d154b68ff4fae3c77632
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2020-07-14 15:50:13 -0500

    Input

--- src/comp/PokedexHeader.js
+++ src/comp/PokedexHeader.js
@@ -1,10 +1,33 @@
 
 import React from 'react';
+import { Input, Segment } from 'semantic-ui-react';
 class PokedexHeader extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      pokeName: null
+    }
+    this.onChange=this.onChange.bind(this)
+  }
+  onChange(event,data) {
+    this.setState({
+      pokeName: data.value
+    })
+  }
+
     render(){
     return (
-        <h1>Hello There</h1>
-    )
+      <div>
+      <Segment>
+      <Input
+        label = {"Input Pokemon Name"}
+        onChange={this.onChange}
+        value = {this.state.pokeName}
+      />
+      </Segment>
+      <h1>{this.state.pokeName}</h1>
+      </div>
+    );
     }
   }
 export default PokedexHeader
