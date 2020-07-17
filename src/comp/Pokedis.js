132f619bd841997ec72babbd40638f5df81e2caf
ryanjw0903 <60299798+ryanjw0903@users.noreply.github.com>
2020-07-17 07:11:20 -0500

    rename routing

--- src/comp/Pokedis.js
+++ src/comp/Pokedis.js
@@ -5,7 +5,7 @@ class Pokedis extends React.Component{
     state = {
         name: '',
         url: '',
-        stats: null
+        stats: null,
     }
     async componentDidMount(){
       const {name, url} = this.props
@@ -16,8 +16,14 @@ class Pokedis extends React.Component{
     render(){
         return (
           <div>
+            {this.state.stats ?  (
+              <div>
               <h1>{this.state.name}</h1>
               <h1>{this.state.stats.forms.name}</h1>
+              </div>
+            ) : (
+              <div>none</div>
+            )}
           </div>
         );
     }
